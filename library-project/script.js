b90261dc74b20ab54ee23f777ed8bc2b5a9a0fab
mariodiazagu <mario.diazagu@gmail.com>
2026-09-15 18:41:44 +0200

    Add song management features with localStorage

--- library-project/script.js
+++ library-project/script.js
@@ -13,24 +13,48 @@ class Cancion {
 
 let misCanciones = [];
 
+//guardar canciones a JSON
+function guardarCanciones(){
+    const cancionesJson = JSON.stringify(misCanciones);
+    localStorage.setItem("canciones", cancionesJson);
+}
+
+
+
 function anadirCancion(titulo, artista, duracion, escuchada){
     const nuevaCancion = new Cancion(titulo, artista, duracion, escuchada);
     misCanciones.push(nuevaCancion);
-}
+    guardarCanciones();
+};
 
-anadirCancion("Cruel Summer", "Taylor Swift", "2:58", true);
-anadirCancion("Liability", "Lorde", "2:56", false);
-anadirCancion("Motion Sickness", "Phoebe Bridgers", "4:19", true);
+let datosGuardados = localStorage.getItem("canciones");
+
+if (datosGuardados) {
+    misCanciones = JSON.parse(datosGuardados);
+    let idMasAlto = misCanciones.reduce(function(acumulador, cancion){
+        if (acumulador > cancion.id){
+            return acumulador;
+        } else {
+            return cancion.id
+        }
+    }, 0);
+    contadorId = idMasAlto + 1;
+}
 
 const contenedorCanciones = document.getElementById("contenedorCanciones");
 
 function mostrarCanciones(canciones){
-    contenedorCanciones.replaceChildren()
+    //primero se vacía lo que ya hay
+    contenedorCanciones.replaceChildren();
+
+    //después muestra todas las canciones
     for (const cancion of canciones){
         let nuevaCancion = document.createElement("div");
         nuevaCancion.dataset.id = cancion.id;
         nuevaCancion.textContent = `${cancion.titulo} - ${cancion.artista} (${cancion.duracion})`;
-        contenedorCanciones.append(nuevaCancion)
+        contenedorCanciones.append(nuevaCancion);
+
+        //boton eliminar cada cancion
         let botonEliminar = document.createElement("button");
         botonEliminar.textContent = "Eliminar"
         nuevaCancion.append(botonEliminar);
@@ -38,8 +62,11 @@ function mostrarCanciones(canciones){
             console.log(botonEliminar.closest("div").dataset.id + " eliminar");
             const idCancionEliminada = Number(botonEliminar.closest("div").dataset.id)
             misCanciones = misCanciones.filter(cancion => cancion.id !== idCancionEliminada);
+            guardarCanciones();
             mostrarCanciones(misCanciones)
         });
+
+        // boton escuchado/no escuchado
         let botonEscuchar = document.createElement("button");
         if (cancion.escuchada){
             botonEscuchar.textContent = "No escuchada"
@@ -52,10 +79,12 @@ function mostrarCanciones(canciones){
             const idCancionElegida = Number(botonEscuchar.closest("div").dataset.id)
             const cancionElegida = misCanciones.find(cancion => cancion.id === idCancionElegida);
             cancionElegida.escuchada = !cancionElegida.escuchada;
+            guardarCanciones();
             mostrarCanciones(misCanciones);
         });
     };
 };
+
 
 const botonNuevaCancion = document.getElementById("botonNuevaCancion");
 const dialogoNuevaCancion = document.getElementById("dialogoNuevaCancion")
